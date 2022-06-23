930ac96e93978229e03ea67c0e6d2a2ffa230385
adrianpiveta <danielpiveta257@gmail.com>
2022-06-22 22:00:10 -0300

    Classes e filiação

--- mutavel.py
+++ mutavel.py
@@ -32,3 +32,55 @@ if __name__ == '__main__':
     print(ComentarioContavel.limite)
     PostagemContavel.limite=4
     postagem4 = PostagemContavel('sou a 4')
+
+class Receita:
+    etapas = []
+    def __init__(self, etapas=None):
+        self.etapas.extend((etapas))
+
+class Alimento(Receita):
+    def __init__(self, etapas=None):
+        super().__init__(etapas)
+
+class Remedio(Receita):
+    def __init__(self, etapas=None):
+        super().__init__(etapas)
+
+if __name__ == '__main__':
+    panetone = Alimento(etapas=['assar em forno quente'])
+    dipirona = Remedio(['adicionar corante'])
+    print(panetone.etapas)
+    print(dipirona.etapas)
+    bolo = Alimento(['Cobertura de murango'])
+    print(bolo.etapas)
+
+
+#metodos estaticos
+class Aluno:
+    def __init__(self, nome):
+        self.nome = nome
+
+class Disciplina:
+    limite_vagas = 20
+    def __init__(self, nome):
+        self.nome = nome
+        self.total_matriculas=0
+        self.matriculados = []
+
+    def matricular(self, aluno):
+        if self.verifica_vagas(self.total_matriculas,
+                               self.limite_vagas):
+            self.matriculados.append(aluno)
+            self.total_matriculas += 1
+        else:
+            print("Turma já está cheia na disciplina {}".format(self.nome))
+
+    @staticmethod
+    def verifica_vagas(total_matriculas, limite_vagas):
+        return limite_vagas > total_matriculas
+
+if __name__ == '__main__':
+    calculo = Disciplina(nome = 'Calculo')
+    jose = Aluno('Jose')
+    calculo.matricular(jose)
+    print(calculo.verifica_vagas(19,20))
