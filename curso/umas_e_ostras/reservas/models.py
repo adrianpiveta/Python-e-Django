6580d940020483b2c51d7c5c79f9d46f7608cffd
adrianpiveta <danielpiveta257@gmail.com>
2022-07-01 22:53:35 -0300

    Introduzindo django -> projeto

---
+++ curso/umas_e_ostras/reservas/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20)
+    email = models.CharField(max_length=200)
+    registrado_em = models.DateTimeField('data do registro')
+
+class Reserva(models.Model):
+    data_reserva = models.DateTimeField('data da reserva')
+    data_evento = models.DateTimeField('data do evento')
+    pessoas = models.IntegerField(default=0)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
---
+++ modelo11.py
@@ -0,0 +1,43 @@
+from datetime import date
+from modelo10 import conectar_banco, criar_cursor, criar_tabela_computador, executar_comando
+
+def inserir_registro_computador(
+        cursor, codigo, nome, aquisicao, vida, marca):
+    comando = 'INSERT INTO computador( '\
+              'codigo, nome, aquisicao, vida, marca' \
+              ') Values (?, ?, ?, ?, ?)'
+    parametros = (codigo, nome, aquisicao, vida, marca)
+    executar_comando(cursor, comando, parametros)
+
+def main():
+    conexao = conectar_banco(':memory:')
+    cursor = criar_cursor(conexao)
+    criar_tabela_computador(cursor)
+    inserir_registro_computador(cursor, 1, 'Vastra', date(2022,7,1), 365, 'acera')
+    inserir_registro_computador(cursor, 2, 'Nitra', date(2020, 7, 1), 365, 'leveio')
+    comando_sql = 'SELECT * FROM computador'
+    registros = executar_comando(cursor, comando_sql)
+    registro_unico = registros.fetchone()
+    print('Registro Unico: \n ', registro_unico)
+    print('OBJETO: ', registros)
+    registro_unico = registros.fetchone()
+    print(registro_unico)
+    comando_sql = "UPDATE computador "\
+                  " set vida = ? where codigo = ?"
+    parametros = (600, 1)
+    registros = executar_comando(cursor, comando_sql, parametros)
+    print(registros.rowcount)
+    parametros=(200, 2)
+    registros =executar_comando(cursor, comando_sql, parametros)
+    print(registros.rowcount)
+    comando_sql = "SELECT * FROM computador"
+    registros = executar_comando(cursor, comando_sql)
+    for registro in registros:
+        print(registro)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
