3459cf75d7e9dca941258d9162e26b4b47ef6b11
adrianpiveta <danielpiveta257@gmail.com>
2022-07-04 23:29:57 -0300

    admin page

--- curso/umas_e_ostras/reservas/admin.py
+++ curso/umas_e_ostras/reservas/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Cliente, Reserva
+
+
+admin.site.register(Reserva)
+
+class ClienteAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Dados pessoais', {'fields': ['nome', 'email', 'telefone', 'endereco']}),
+        ('Datas', {
+            'fields': ['registrado_em'],
+            'classes': ['collapse']}), #pode ser expandida, por padrão abre minimizada
+    ]
+
+admin.site.register(Cliente, ClienteAdmin) # registra cliente como gerenciavel pelo painel AD
--- curso/umas_e_ostras/reservas/models.py
+++ curso/umas_e_ostras/reservas/models.py
@@ -1,17 +1,29 @@
 import datetime
 
 from django.db import models
+from django.utils import timezone
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=200)
     endereco = models.CharField(max_length=200)
     telefone = models.CharField(max_length=20)
     email = models.CharField(max_length=200)
-    registrado_em = models.DateTimeField('data do registro', default=datetime.date)
+    registrado_em = models.DateTimeField('data do registro')
+
+    def __str__(self):
+        return self.nome
+
+    def registro_eh_antigo(self):
+        um_ano = timezone.now() - datetime.timedelta(days=365)
+        return self.registrado_em < um_ano # retorna true or false se for antigo ou não
 
 class Reserva(models.Model):
     data_reserva = models.DateTimeField('data da reserva')
     data_evento = models.DateTimeField('data do evento')
     pessoas = models.IntegerField(default=0)
+
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.cliente, str(self.data_evento))
 
