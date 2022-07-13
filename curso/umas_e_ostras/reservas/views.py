406d2457e921dbe4647f9ea7f3d000f6fc25e7fe
adrianpiveta <danielpiveta257@gmail.com>
2022-07-12 22:45:31 -0300

    render

--- curso/umas_e_ostras/reservas/views.py
+++ curso/umas_e_ostras/reservas/views.py
@@ -1,13 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+from django.template.loader import render_to_string
 from django.shortcuts import render
 
+from .models import Cliente
+
 def index(request):
-    return HttpResponse('Bem vindo@ ao sistema de reservas '\
-                        'umas e ostras')
+    ultimos_clientes = Cliente.objects.order_by(
+        '-registrado_em')[:5]
+    context = {
+        'ultimos_clientes': ultimos_clientes,
+    }
+    template = render_to_string('reservas/index.html',context=context)
+    #return HttpResponse(template)
+    return  render(request, 'reservas/index.html', context)
 
 def detalhe(request, cliente_id):
-    return HttpResponse('A requisição foi feita pelo cliente: {} '\
-                        ''.format(cliente_id))
+    try:
+        cliente = Cliente.objects.get(pk=cliente_id)
+    except:
+        raise Http404("Cliente inexistente")
+    return render(request, 'reservas/detalhe.html', {'cliente':cliente})
 
 def reservas(request, cliente_id):
     resposta = 'Foi feito uma solicitação para os reservas '\
